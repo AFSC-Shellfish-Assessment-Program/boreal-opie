7cc6d2075c4c860f6830dbd31f5f362094d0dfb0
Erin-Fedewa-NOAA <erin.fedewa@noaa.gov>
2022-04-13 21:21:40 -0800

    Adding immature core area and avg temp datasets

--- Scripts/Area_90.R
+++ Scripts/Area_90.R
@@ -1,6 +1,9 @@
 # notes ----
 
-# Determine stations that make up 90% of immature/immature female snow crab CPUE 
+# Determine core habitat of immature snow crab in EBS
+  #1) Determine stations that compose average core habitat across the long-term timeseries 
+  #2) Determine average bottom temperature of core habitat within each yr
+  
 # Author: Erin Fedewa
 
 # load ----
@@ -13,7 +16,7 @@ library(ggmap)
 sc_catch <- read.csv("./Data/crabhaul_opilio.csv", skip=5)
 
 #EBS strata data ----
-strata_ebs <- read_csv("./Data/crabstrata_opilio.csv")
+sc_strata <- read_csv("./Data/crabstrata_opilio.csv")
 
 ###################################################
 # data exploration ----
@@ -38,82 +41,71 @@ ggplot() +
 
 ###################################################
 
-# data  ----
-
-#CPUE by station for all immature snow crab 
+#Calculate CPUE by station for all immature snow crab 
 sc_catch %>%
   filter(HAUL_TYPE == 3, 
-         SEX %in% 1:2) %>%
+         SEX %in% 1:2,
+         AKFIN_SURVEY_YEAR > 1979) %>%
   mutate(MAT_SEX = case_when((SEX == 1 & WIDTH < 95 & SHELL_CONDITION <= 2) ~ "Immature Male",
                              (SEX == 2 & CLUTCH_SIZE == 0) ~ "Immature Female")) %>%
   filter(MAT_SEX %in% c("Immature Male", "Immature Female")) %>%
-  group_by(AKFIN_SURVEY_YEAR, GIS_STATION, AREA_SWEPT,MID_LATITUDE, MID_LONGITUDE) %>%
+  group_by(AKFIN_SURVEY_YEAR, GIS_STATION, AREA_SWEPT,MID_LATITUDE, MID_LONGITUDE, GEAR_TEMPERATURE) %>%
   summarise(N_CRAB = sum(SAMPLING_FACTOR, na.rm = T),
             CPUE = N_CRAB / mean(AREA_SWEPT)) %>%
-# compute 90% of habitat for immature snow crab
-# i.e. list of stations contributing to 90% of cumulative cpue within a given year 
+  #join to zero catch stations
+  right_join(sc_strata %>%
+                    filter(SURVEY_YEAR > 1979) %>%
+                    distinct(SURVEY_YEAR, STATION_ID, STRATUM, TOTAL_AREA_SQ_NM) %>%
+                    rename_all(~c("GIS_STATION", "AKFIN_SURVEY_YEAR",
+                             "STRATUM", "TOTAL_AREA_SQ_NM"))) %>%
+              replace_na(list(CPUE = 0)) -> cpue
+#####
+  
+#1) Determine stations that compose average core habitat across the long-term timeseries 
+
+#stations in 10-100 CPUE percentile range
+cpue %>%
+  group_by(GIS_STATION) %>%
+  summarise(AVG_CPUE = mean(CPUE)) %>%
+  filter(AVG_CPUE > quantile(AVG_CPUE, 0.10)) -> perc10 #331 stations 
+  
+#stations in 20-100 CPUE percentile range
+cpue %>%
+  group_by(GIS_STATION) %>%
+  summarise(AVG_CPUE = mean(CPUE)) %>%
+  filter(AVG_CPUE > quantile(AVG_CPUE, 0.20)) -> perc20 #300 stations
+
+#stations in 50-100 CPUE percentile range
+cpue %>%
+  group_by(GIS_STATION) %>%
+  summarise(AVG_CPUE = mean(CPUE)) %>%
+  filter(AVG_CPUE > quantile(AVG_CPUE, 0.50)) -> perc50 #187 stations
+  
+#Write csv for stations in 50th percentile of avg CPUE  
+write.csv(perc50, file="./Data/imm_area_50perc.csv")
+
+######
+
+#2) Determine average bottom temperature of core habitat within each yr
+cpue %>%
   group_by(AKFIN_SURVEY_YEAR) %>%
-  filter(CPUE > quantile(CPUE, 0.10)) -> area90 #return stations with density > 10th percentile 
+  filter(CPUE > quantile(CPUE, 0.50)) %>%
+  summarise(AVG_TEMP = mean(GEAR_TEMPERATURE, na.rm = T)) -> avg_temp
+
+#Write csv for avg bottom temp within 50th percentile home range   
+write.csv(avg_temp, file="./Data/avg_temp_50perc.csv")
+
+
+
+
+
+
+
+
+
+
+
+
   
-#Plot
-area90 %>%
-  filter(AKFIN_SURVEY_YEAR > 2010) %>%
-  group_by(AKFIN_SURVEY_YEAR, MID_LONGITUDE, MID_LATITUDE) %>%
-  distinct(GIS_STATION) %>%
-  ggplot() +
-  geom_point(aes(x = MID_LONGITUDE, y = MID_LATITUDE), size=.5) +
-  labs(x = "Longitude", y = "Latitude") +
-  facet_wrap(~AKFIN_SURVEY_YEAR)
-
-#right skewed data (should we be including 0 catch stations)-choose different percentile?
-  #uninformative b/c caught at  alot of stations 
-  
-  
-  
   
 
-
-
-# function to compute 
-f_area90_est <- function(x){
-  x %>%
-    group_by(AKFIN_SURVEY_YEAR) %>%
-    arrange(-CPUE) %>% #sort by cpue (large:small)
-    mutate(PROP_CPUE = CPUE/sum(CPUE),  #calculate the proportion of total cpue for each station
-           cum_cpue = cumsum(PROP_CPUE)) %>%  
-    filter(cum_cpue <= 0.90) %>% #T if in area90, F if not
-    count() %>%
-    mutate(d90 = (n + 1) * 401) %>% #add 1 station to n to push over 95%
-    pull(d95)
-  }
-
-# do the estimation for immature snow crab
-cpue_long %>%
-  filter(!(Station %in% corner)) %>% #exclude corner stations
-  group_by(Station, size_sex) %>%
-  filter(n() == 32) %>% #include only stations sampled every year in 32 year timeseries
-  ungroup() %>% 
-  nest(-year, -size_sex) %>%
-  mutate(d95 = purrr::map_dbl(data, f_d95_est)) %>% #apply d95 function to each element 
-  unnest() %>%
-  group_by(year, size_sex) %>%
-  summarise(cpue = sum(num_crab) / sum(area_swept), # add a column for total cpue of each group in each year
-            d95 = mean(d95))->d95 # take 'mean' just to get one value (they are all the same)
-
-# do the estimation for immature female snow crab only
-cpue_long %>%
-  filter(!(Station %in% corner)) %>% #exclude corner stations
-  group_by(Station, size_sex) %>%
-  filter(n() == 32) %>% #include only stations sampled every year in 32 year timeseries
-  ungroup() %>% 
-  nest(-year, -size_sex) %>%
-  mutate(d95 = purrr::map_dbl(data, f_d95_est)) %>% #apply d95 function to each element 
-  unnest() %>%
-  group_by(year, size_sex) %>%
-  summarise(cpue = sum(num_crab) / sum(area_swept), # add a column for total cpue of each group in each year
-            d95 = mean(d95))->d95 # take 'mean' just to get one value (they are all the same)
-
-cbind()
-
-write.csv(d95, file="./Output/D95_output.csv")
-  
