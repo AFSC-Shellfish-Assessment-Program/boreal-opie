4cc9283fe669af042482588e0be32647b1fa1f3d
mikelitzow <mike.litzow@noaa.gov>
2022-05-24 08:22:13 -0800

    More iterations, higher adapt delta for mice_brm

--- Scripts/missing_borealization_effects.R
+++ Scripts/missing_borealization_effects.R
@@ -188,9 +188,9 @@ form <- bf(log_abundance_lead1 ~ log_abundance + s(trend))
 ## fit
 mice_brm <- brm_multiple(form,
               data = imputed.data,
-              cores = 4, chains = 4, iter = 2000,
+              cores = 4, chains = 4, iter = 3000,
               save_pars = save_pars(all = TRUE),
-              control = list(adapt_delta = 0.99999, max_treedepth = 10))
+              control = list(adapt_delta = 0.999999, max_treedepth = 10))
 
 saveRDS(mice_brm, file = "output/mice_brm.rds")
 
