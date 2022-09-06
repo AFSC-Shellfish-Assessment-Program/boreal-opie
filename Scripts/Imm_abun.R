5c3a275bbd80c09355bf350e2b4ae2211800dfd8
mikelitzow <mike.litzow@noaa.gov>
2022-09-06 10:29:38 -0800

    Updating to run 2022 data, cleaning up to use in-house data (not AKFIN)

--- Scripts/Imm_abun.R
+++ Scripts/Imm_abun.R
@@ -14,36 +14,44 @@ library(ggmap)
 ##############################################
 
 ## EBS haul data ----
-sc_catch <- read.csv("./Data/crabhaul_opilio.csv", skip=5)
+sc_catch <- read.csv("./Data/crabhaul_opilio.csv")
 
 #EBS strata data ----
-sc_strata <- read_csv("./Data/crabstrata_opilio.csv")
+sc_strata <- read_csv("./Data/STRATA_OPILIO_NEWTIMESERIES.csv")
 
 ###################################################
+
+# define survey year in catch
+sc_catch <- sc_catch %>%
+  mutate(YEAR = floor(CRUISE/100))
+
+# rename year in strata
+sc_strata <- sc_strata %>%
+  rename(YEAR = SURVEY_YEAR)
 
 #Calculate CPUE by station for immature snow crab 
 sc_catch %>%
   filter(HAUL_TYPE == 3, 
          SEX %in% 1:2,
-         AKFIN_SURVEY_YEAR > 1979) %>%
+         YEAR > 1979) %>%
   mutate(MAT_SEX = case_when((SEX == 1 & WIDTH < 95) ~ "Immature Male",
                              (SEX == 2 & CLUTCH_SIZE == 0) ~ "Immature Female")) %>%
   filter(MAT_SEX %in% c("Immature Male", "Immature Female")) %>%
-  group_by(AKFIN_SURVEY_YEAR, GIS_STATION, AREA_SWEPT,MID_LATITUDE, MID_LONGITUDE) %>%
+  group_by(YEAR, GIS_STATION, AREA_SWEPT,MID_LATITUDE, MID_LONGITUDE) %>%
   summarise(N_CRAB = sum(SAMPLING_FACTOR, na.rm = T),
             CPUE = N_CRAB / mean(AREA_SWEPT)) %>%
   #join to zero catch stations
   right_join(sc_strata %>%
-               filter(SURVEY_YEAR > 1979) %>%
-               distinct(SURVEY_YEAR, STATION_ID, STRATUM, TOTAL_AREA_SQ_NM) %>%
-               rename_all(~c("GIS_STATION", "AKFIN_SURVEY_YEAR",
+               filter(YEAR > 1979) %>%
+               distinct(YEAR, STATION_ID, STRATUM, TOTAL_AREA) %>%
+               rename_all(~c("GIS_STATION", "YEAR",
                              "STRATUM", "TOTAL_AREA_SQ_NM"))) %>%
   replace_na(list(CPUE = 0)) %>%
   #Scale to abundance by strata
-  group_by(AKFIN_SURVEY_YEAR, STRATUM, TOTAL_AREA_SQ_NM) %>%
+  group_by(YEAR, STRATUM, TOTAL_AREA_SQ_NM) %>%
   summarise(MEAN_CPUE = mean(CPUE, na.rm = T),
             ABUNDANCE = (MEAN_CPUE * mean(TOTAL_AREA_SQ_NM))) %>%
-  group_by(AKFIN_SURVEY_YEAR) %>%
+  group_by(YEAR) %>%
   #Sum across strata
   summarise(ABUNDANCE_MIL = sum(ABUNDANCE)/1e6) -> imm_abundance
 
