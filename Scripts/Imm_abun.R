850e1dc0fc1fefa4eb6b8bb7127bbe657330cb1a
Emily Ryznar <emily.ryznar@nmfs.local>
2022-11-30 10:25:57 -0900

    updating with new timeseries

--- Scripts/Imm_abun.R
+++ Scripts/Imm_abun.R
@@ -15,6 +15,8 @@ library(ggmap)
 
 ## EBS haul data ----
 sc_catch <- read.csv("./Data/crabhaul_opilio.csv")
+sc_catch <- read.csv("./Data/CRABHAUL_OPILIO.csv")
+
 
 #EBS strata data ----
 sc_strata <- read_csv("./Data/STRATA_OPILIO_NEWTIMESERIES.csv")
