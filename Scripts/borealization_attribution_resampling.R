b9e0f41aee9bc760277f5985affd9275589f8988
mikelitzow <mike.litzow@noaa.gov>
2023-07-29 00:30:58 -0800

    Using same resampling apporach for probabilities as used for borealization pdfs; only using observed borealization values, rather than draws from sst-borealization model for observations

--- Scripts/borealization_attribution_resampling.R
+++ Scripts/borealization_attribution_resampling.R
@@ -7,11 +7,18 @@ library(tidyverse)
 theme_set(theme_bw())
 
 # load observed borealization events 
-# drawn from SST-borealization posteriors applied to observed SST values
-observed.borealization <- read.csv("./output/observed_borealization_from_posteriors.csv")
+# load DFA model
+mod <- readRDS("./output/DFA_model.rds")
+
+# calculate DFA trend (borealization index)
+observed.borealization <- data.frame(year=1972:2022,
+                    borealization_index = as.vector(mod$states))
+
 
 # load cmip6 historical draws from the same model
 historical.borealization <- read.csv("./output/cmip_historical_borealization_draws.csv")
+
+nrow(historical.borealization)
 
 # load preindustrial CMIP6 anomalies and make draws from posteriors
 cmip.anom <- read.csv("./data/CMIP6.anomaly.time.series.csv") %>%
@@ -20,15 +27,18 @@ cmip.anom <- read.csv("./data/CMIP6.anomaly.time.series.csv") %>%
     select(year, model, annual.unsmoothed) %>%
     rename(sst.anomaly = annual.unsmoothed)
 
+# load brms model
+sst_boreal_brm <- readRDS("./output/sst_boreal_brm.rds")
+
 # estimate borealization for each CMIP6 SST anomaly from model posteriors
 preindustrial.borealization <- data.frame()
 
 for(i in 1:nrow(cmip.anom)){
-    
+    print(i)
     temp <- data.frame(model = cmip.anom$model[i],
                        borealization_index = posterior_predict(sst_boreal_brm,
                                                                newdata = cmip.anom[i,],
-                                                               ndraws = 1)) #  1 draw each, as we have n = 250 preindustrial observations for each model
+                                                               nsamples = 1)) #  1 draw each, as we have n = 250 preindustrial observations for each model
     preindustrial.borealization <- rbind(preindustrial.borealization,
                                        temp)
     
@@ -36,6 +46,11 @@ for(i in 1:nrow(cmip.anom)){
 
 nrow(preindustrial.borealization) 
 
+# save
+write.csv(preindustrial.borealization,
+          "./output/cmip_preindustrial_historical_borealization_draws.csv", row.names = F)
+
+preindustrial.borealization <- read.csv("./output/cmip_preindustrial_historical_borealization_draws.csv")
 
 # load model weights 
 weights <- read.csv("./data/normalized_CMIP6_weights.csv")
@@ -50,6 +65,9 @@ weights <- weights %>%
 model.warming.trends <- read.csv("./data/CMIP6_brms_warming_rate.csv")
 
 # check model names
+# get vector of model names
+models <- unique(cmip.anom$model)
+
 check <- data.frame(models = models,
                     trend.names = unique(model.warming.trends$model))
 check # line up fine
@@ -61,11 +79,9 @@ predicted.warming <- read.csv("./data/brms_predicted_North_Pac_warming.csv")
 # load estimated warming level timing for each model
 timing <- read.csv("./Data/model.north.pacific.warming.timing.csv")
 
-# get vector of model names
-models <- unique(cmip.anom$model)
 
-# create df to catch preindustrial and historical probabilities for each borealization observation
-probs <- data.frame()
+# create df to catch preindustrial and historical outcomes (greater than or not) for each borealization observation
+outcomes <- data.frame()
 
 # loop through each observation
 for(h in 1:nrow(observed.borealization)){
@@ -74,7 +90,7 @@ for(h in 1:nrow(observed.borealization)){
     obs.temp <- observed.borealization[h,]
     
     # define 15-year window
-    window <- (obs.temp$year[k] - 7) : (obs.temp$year[k] + 7)
+    window <- (obs.temp$year - 7) : (obs.temp$year + 7)
     
     # define range of predicted warming values for this window
     warming.range <- range(predicted.warming$pred_mean[predicted.warming$year %in% window])
@@ -89,14 +105,14 @@ for(i in 1:length(models)){ # start i loop (models)
         filter(model == models[i])
     
     
-    # record preindustrial probability
-    probs <- rbind(probs,
+    # record preindustrial outcomes
+    outcomes <- rbind(outcomes,
                                 data.frame(year = obs.temp$year,
                                            observed_borealization = obs.temp$borealization_index,
                                             model = models[i],
                                            period = "preindustrial",
                                            n = 250,
-                                           probabililty = sum(pre.temp$borealization_index >= obs.temp$borealization_index)/250))
+                                           outcome = if_else(pre.temp$borealization_index >= obs.temp$borealization_index, 1, 0)))
     
     
     ## record historical outcomes 
@@ -115,158 +131,116 @@ for(i in 1:length(models)){ # start i loop (models)
         filter(model == models[i],
                year %in% use$year)
     
-    probs <- rbind(probs,
+    if(nrow(hist.temp.use) > 0){ # record outcomes only if we have observations in 1940:2040 matching this warming window
+    
+    outcomes <- rbind(outcomes,
                    data.frame(year = obs.temp$year,
                               observed_borealization = obs.temp$borealization_index,
                               model = models[i],
-                              period = "historical",
                               n = nrow(hist.temp.use),
-                              probabililty = sum(hist.temp.use$borealization_index >= obs.temp$borealization_index)/nrow(hist.temp.use)))
+                              period = "historical",
+                              outcome = if_else(hist.temp.use$borealization_index >= obs.temp$borealization_index, 1, 0)))
+    }
     
 } # close i loop (models) 
     
 } # close h loop (observations)
     
 
-
 # save
-write.csv(probs, "./output/borealization_probabilities.csv", row.names = F)
+write.csv(outcomes, "./output/borealization_outcomes_for_attribution.csv", row.names = F)
 
-probs <- read.csv("./output/borealization_probabilities.csv")
+outcomes <- read.csv("./output/borealization_outcomes_for_attribution.csv")
 
+# join with model weights
+outcomes <- left_join(outcomes, weights) %>%
+    mutate(total_weight = model_weight/n)
 
-# summarize!
+# resample and calculate FAR!
 
 # create df for output
-summarized_probabilities <- data.frame()
+resampled_FAR <- data.frame()
 
-# remove n = 0 cases and calcuate # of cases of greater borealization
-probs <- probs %>%
-    filter(n > 0) %>%
-    mutate(positive_cases = n * probabililty)
-
-summarized_probabilities <- probs %>%
-    group_by(year, period, model) %>%
-    summarise(n = sum(n), 
-              positive_cases = sum(positive_cases)) %>%
-    mutate(probabililty = positive_cases / n)
-
-
-summarized_probabilities <- left_join(summarized_probabilities, weights) %>%
-    mutate(total_weight = n*model_weight)
 
 # now resample
-periods <- unique(summarized_probabilities$period)
-resampled_probs <- data.frame()
+periods <- unique(outcomes$period)
 
-for(p in 1:2){
-    # p <- 1
-    for(y in 1950:2022){
-     # y <- 1950
+for(y in 1972:2022){
+    # y <- 1972
+    for(x in 1:1000){ # go through 1000 iterations
+        # x <- 1
+    # start with preindustrial probability
+     p <- 1
     
-     temp <- summarized_probabilities %>%
+     temp <- outcomes %>%
         filter(period == periods[p],
                year == y) 
-        
-     resampled_probs <- rbind(resampled_probs,
-                              data.frame(year = y,
-                                         period = periods[p],
-                                         probability = weighted.mean(temp$probabililty, w = temp$total_weight)))   
-        
+     
+     # resample outcomes 1000 times
+     
+     preind_resample <- sample(temp$outcome, 1000, replace = T, prob = temp$total_weight)
+     preind_prob <- sum(preind_resample)/1000
+     
+     # now historical probability
+     p <- 2
+     
+     temp <- outcomes %>%
+         filter(period == periods[p],
+                year == y) 
+     
+     # resample outcomes 1000 times
+     
+     hist_resample <- sample(temp$outcome, 1000, replace = T, prob = temp$total_weight)
+     
+     hist_prob <- sum(hist_resample)/1000
+     
+     # and record results for this iteration
+     resampled_FAR <- rbind(resampled_FAR,
+                            data.frame(year = y,
+                                       draw = x,
+                                       FAR = 1 - (preind_prob/hist_prob)))
+     
+     
     }
     
 }
 
 
-wide_probs <- resampled_probs %>%
-    pivot_wider(names_from = period, 
-                values_from = probability) %>%
-    mutate(FAR = 1 - (preindustrial/historical),
-           RR = 1/(1-FAR))
+
+# and summarize results
+attribution_stats <- data.frame()
+
+for(y in 1972:2022){
+    
+temp <- resampled_FAR %>%
+    filter(year == y)
+
+attribution_stats <- rbind(attribution_stats,
+                           data.frame(year = y,
+                                      FAR = mean(temp$FAR),
+                                      LCI_FAR = quantile(temp$FAR, 0.025), 
+                                      UCI_FAR = quantile(temp$FAR, 0.975)))
+
+}
+
+ggplot(attribution_stats, aes(year, FAR)) +
+    geom_point() +
+    geom_line() +
+    geom_ribbon(aes(ymin = LCI_FAR,
+                      ymax = UCI_FAR), fill = "dark grey", alpha=0.3)
+
+# RR
+attribution_stats <- attribution_stats %>%
+    mutate(RR = 1/(1-FAR),
+           LCI_RR = 1/(1-LCI_FAR),
+           UCI_RR = 1/(1-UCI_FAR))
+
+ggplot(attribution_stats, aes(year, RR)) + # no error ribbon b/c UCI is undefined if FAR = 1
+    geom_point() +
+    geom_line() 
+
 
 # save 
 
-write.csv(wide_probs, "./output/probabilistic_attribution_stats.csv", row.names = F)
+write.csv(attribution_stats, "./output/probabilistic_attribution_stats.csv", row.names = F)
 
-ggplot(wide_probs, aes(year, FAR)) +
-    geom_point() +
-    geom_line()
-
-ggplot(wide_probs, aes(year, RR)) +
-    geom_point() +
-    geom_line()
-
-
-
-wide_probs$FAR <- 1 - (preindustrial)
-
-resample.pdf <- data.frame()
-
-periods <- unique(borealization.pdfs$period)
-
-for(i in 1:length(periods)){
-    # i <- 1
-    
-    temp <- borealization.pdfs[borealization.pdfs$period == periods[i],]
-    
-    resample.pdf <- rbind(resample.pdf,
-                          data.frame(period = periods[i],
-                                     borealization_index = sample(temp$borealization_index, 1000, replace = T, prob = temp$normalized_weight)))
-    
-}
-
-# reorder
-plot.order <- data.frame(period = unique(resample.pdf$period),
-                         order = 1:5)
-
-
-resample.pdf <- left_join(resample.pdf, plot.order) %>%
-    mutate(period =  reorder(period, order))
-
-
-sum <- resample.pdf %>%
-    group_by(period) %>%
-    summarize(proportion = sum(borealization_index > 2) / n()) # using 1.5 as a critical value
-
-sum
-
-# and plot
-theme_set(theme_bw())
-cb <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
-boreal.pdf.plot <- ggplot(resample.pdf, aes(period, borealization_index)) +
-    geom_violin(fill = cb[6], lty = 0, alpha = 0.5) +
-    coord_flip(ylim = c(-4.5, 4.5)) +
-    xlab("North Pacific warming") +
-    ylab("Borealization index") +
-    geom_hline(yintercept = 2, lty = 2) +
-    scale_x_discrete(labels = c("Preindustrial",
-                                "1950 to 0.5°",
-                                "0.5° to 1.0°  
-                              (~2003 - 2019)",
-                                "1.0° to 1.5°  ,
-                              (~2019 - 2038)",
-                                "1.5° to 2.0°  
-                              (~2038 - 2060)")) +
-    scale_y_continuous(breaks = seq(-4,4, by = 2))
-
-
-boreal.pdf.plot
-
-
-###################
-## Calc FAR
-far <- 1 - (pre_prob / his_prob)
-range(far, na.rm = TRUE)
-
-
-far_pred <- data.frame(annual.anomaly.3yr = nd_pre$annual.anomaly.3yr,
-                       prob = apply(far, 2, mean),
-                       lower = apply(far, 2, quantile, probs = 0.025),
-                       upper = apply(far, 2, quantile, probs = 0.975))
-
-g <- ggplot(far_pred) +
-    geom_hline(yintercept = 0, color = "grey50", linetype = 2) +
-    geom_line(aes(x = annual.anomaly.3yr, y = prob), size = 0.8) +
-    geom_ribbon(aes(x = annual.anomaly.3yr, ymin = lower, ymax = upper), alpha = 0.15)
-print(g)
