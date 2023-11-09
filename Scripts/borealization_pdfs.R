29e9c5c67c5156f135f4f7769323e3f37b91a68e
mikelitzow <mike.litzow@noaa.gov>
2023-11-08 21:08:53 -0900

    Increased resampling to 100,000 draws for projection (Fig. 2c) to improve estimates of tail values; increased diagnostic

--- Scripts/borealization_pdfs.R
+++ Scripts/borealization_pdfs.R
@@ -177,6 +177,13 @@ check <- borealization.pdfs %>%
 
 check # instances differ among models, need to account for this!
 
+# check outcomes per period
+check <- borealization.pdfs %>%
+  group_by(period) %>%
+  summarise(count = n())
+
+check
+
 borealization.pdfs <- left_join(borealization.pdfs, check)
 
 # load CMIP6 model weights
@@ -192,20 +199,55 @@ borealization.pdfs <- left_join(borealization.pdfs, model.weights) %>%
   mutate(total_weight = normalized_weight/count)
 
 # resample to weight models
-resample.pdf <- data.frame()
+resample.pdf <- resample_density <- data.frame()
 
 periods <- unique(borealization.pdfs$period)
 
+set.seed(99)
 for(i in 1:length(periods)){
   # i <- 1
   
   temp <- borealization.pdfs[borealization.pdfs$period == periods[i],]
   
-  resample.pdf <- rbind(resample.pdf,
-                        data.frame(period = periods[i],
-                                   borealization_index = sample(temp$borealization_index, 1000, replace = T, prob = temp$total_weight)))
+  # for(j in 1:1000){ # 1000 density estimates from each period (each defined by 1000 draws from the relevant borealization_index estimates)
+
   
-}
+  temp_borealization_index <- sample(temp$borealization_index, 100000, replace = T, prob = temp$total_weight)
+    
+  resample.pdf <- rbind(resample.pdf,
+  data.frame(period = periods[i],
+  borealization_index = temp_borealization_index))
+
+  # temp_density <- density(temp_borealization_index)
+  # 
+  # resample_density <- rbind(resample_density,
+  #                           data.frame(period = periods[i],
+  #                                      draw = j,
+  #                                      x = temp_density$x,
+  #                                      y = temp_density$y))
+  
+  # } # close j loop (density estimates)
+
+} # close i loop (periods)
+
+# # place x values in common set of bins to compare across draws
+# bin.min <- round(min(resample_density$x),2)-0.005
+# bin.max <- round(max(resample_density$x),2)+0.005
+# 
+# bins <- seq(from = bin.min,
+#             to = bin.max, 
+#             by = 0.01)
+# 
+# resample_density <- resample_density %>%
+#   mutate(binned_x = cut(x, breaks = bins))
+#   
+# 
+# sum_density
+# 
+# mean_density <- resample_density %>%
+#   mutate(x = round(x, 2)) %>%
+#   group_by(period, x) %>%
+#   dplyr::summarise(y = mean(y))
 
 # reorder
 plot.order <- data.frame(period = unique(resample.pdf$period),
@@ -214,6 +256,7 @@ plot.order <- data.frame(period = unique(resample.pdf$period),
 
 resample.pdf <- left_join(resample.pdf, plot.order) %>%
   mutate(period =  reorder(period, order))
+
 
 # save
 write.csv(resample.pdf, "./output/resampled_borealization_pdfs.csv", row.names = F)
@@ -229,9 +272,21 @@ sum <- resample.pdf %>%
 
 sum
 
+# quick plot
+quick_plot <- data.frame(warming = c(0.75, 1.25, 1.75, 0.25, 0),
+                         prob_change = sum$proportion_high/min(sum$proportion_high))
+
+ggplot(quick_plot, aes(warming, prob_change)) +
+  geom_line() +
+  geom_point()
+
 # and plot
 theme_set(theme_bw())
 cb <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# reorder periods
+resample.pdf <- left_join(resample.pdf, plot.order) %>%
+  mutate(period =  reorder(period, order))
 
 boreal.pdf.plot <- ggplot(resample.pdf, aes(period, borealization_index)) +
   geom_hline(yintercept = c(-1,2), color = cb[c(6,7)], lty = 2, alpha = 0.8) +
